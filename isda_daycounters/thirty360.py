b69c9c15fed801d32d68ae13fdabbce4f278df71
kg1128 <kg1128@gmail.com>
2017-07-19 14:28:20 -0400

    Update to thirty360.py
    Corrected day_count calculation for 30/360.
    
    Source:
    http://www.hsbcnet.com/gbm/attachments/standalone/2006-isda-definitions.pdf (file 4.16.f)
    or
    https://en.wikipedia.org/wiki/Day_count_convention#30.2F360_Bond_Basis

--- isda_daycounters/thirty360.py
+++ isda_daycounters/thirty360.py
@@ -12,9 +12,12 @@ name = 'thirty/360'
 
 def day_count(start_date, end_date):
     """Returns number of days between start_date and end_date, using Thirty/360 convention"""
+    d1 = min(30, start_date.day)
+    d2 = min(d1, end_date.day) if d1 == 30 else end_date.day
+    
     return 360*(end_date.year - start_date.year)\
-           + 30*(end_date.month - start_date.month - 1)\
-           + max(0, 30 - start_date.day) + min(30, end_date.day)
+           + 30*(end_date.month - start_date.month)\
+           + d2 - d1
 
 
 def year_fraction(start_date, end_date):
