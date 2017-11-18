604897a4746853c7987650afe8c15692802ab7a6
Mitchell Snaith <msnaith@edu.uwaterloo.ca>
2017-11-17 19:00:04 -0500

    fix actualactual year_fraction to handle datetime.datetimes

--- isda_daycounters/actualactual.py
+++ isda_daycounters/actualactual.py
@@ -7,7 +7,7 @@ http://www.hsbcnet.com/gbm/attachments/standalone/2006-isda-definitions.pdf
 """
 
 import calendar
-import datetime
+import datetime as dt
 
 
 name = 'actual/actual'
@@ -22,16 +22,19 @@ def year_fraction(start_date, end_date):
     """Returns fraction in years between start_date and end_date, using Actual/Actual convention"""
     if start_date == end_date:
         return 0.0
-    if start_date > end_date:
-        return - year_fraction(end_date, start_date)
+
+    start_date = dt.datetime.combine(start_date, dt.datetime.min.time())
+    end_date = dt.datetime.combine(end_date, dt.datetime.min.time())
+    
     start_year = start_date.year
     end_year = end_date.year
     year_1_diff = 366 if calendar.isleap(start_year) else 365
     year_2_diff = 366 if calendar.isleap(end_year) else 365
 
     total_sum = end_year - start_year - 1
-    diff_first = datetime.date(start_year + 1, 1, 1) - start_date
+    diff_first = dt.datetime(start_year + 1, 1, 1) - start_date
     total_sum += diff_first.days / year_1_diff
-    diff_second = end_date - datetime.date(end_year, 1, 1)
+    diff_second = end_date - dt.datetime(end_year, 1, 1)
     total_sum += diff_second.days / year_2_diff
+
     return total_sum
